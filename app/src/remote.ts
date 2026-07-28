ff2e9441fee409978cb021e3f26beba628ee9742
Etavioxy <etavioxy@foxmail.com>
2026-07-28 11:11:15 +0800

    wip: (debug) [#9] WS render_component handler也加fetch到EventBuffer

--- app/src/remote.ts
+++ app/src/remote.ts
@@ -61,6 +61,7 @@ export class RemoteBridge implements Bridge {
           if (msg.state) this.topStateListeners.forEach((cb) => cb(msg.state!));
           break;
         case "render_component":
+          fetch("http://127.0.0.1:47600/events", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ desc: `[WS] render_component: ${msg.spec?.id ?? "no-id"}` }) }).catch(() => {});
           if (msg.spec) this.renderListeners.forEach((cb) => cb(msg.spec!));
           break;
         case "queue_changed":
